94ab356f15791ba15dca25647b4046222a44fd53
Augustus Kirby <kirbyaugustus@gmail.com>
2019-04-17 03:12:09 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+# def square_array(array)
+#   new_array = []
+#   array.each do |num|
+#   new_array << num ** 22
+#
+# end
+# end
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num| new_array << num ** 2
+end
+return new_array
+
 end
